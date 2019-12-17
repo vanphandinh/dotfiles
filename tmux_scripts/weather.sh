4f56b4625dd9f3692e00fe2216c91493ecbc2a82
Phan Dinh Van <vanphandinh@outlook.com>
2019-12-17 10:28:21 +0700

    add updating city manually

--- tmux_scripts/weather.sh
+++ tmux_scripts/weather.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo $(curl -s wttr.in/\?format\="%l:+%c%20%t%20%w&period=5")
+CITY=$(echo $1 | sed -e "s/ /%20/g")
+echo $(curl -s wttr.in/$CITY\?format\="%l:+%c%20%t%20%w&period=5")
