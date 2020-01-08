2f381d0a7f15afa84902ee8501b2832b47251877
Phan Dinh Van <vanphandinh@outlook.com>
2020-01-08 08:46:28 +0700

    add disabling weather plugin feature

--- tmux_scripts/weather.sh
+++ tmux_scripts/weather.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-CITY=$(echo $1 | sed -e "s/ /%20/g")
-echo $(curl -s wttr.in/$CITY\?format\="%l:+%c%20%t%20%w&period=5")
+if [ "$1" == "off" ]; then
+    echo ""
+else
+    CITY=$(echo $1 | sed -e "s/ /%20/g")
+    echo $(curl -s wttr.in/$CITY\?format\="%l:+%c%20%t%20%w&period=5")
+fi
